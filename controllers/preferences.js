84a2bed4056cd58fa7da2b9b4f7a9546c5903b7c
Luciano Nicolás <luciano.prina@hotmail.es>
2020-11-28 09:44:20 -0300

    Solucionamos el error que no conectaba con la lista de preferencias

--- controllers/preferences.js
+++ controllers/preferences.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 var path = require('path');
 
 var controller = {
-    savePreferences: function(req, res){
+    savePreferences: function (req, res) {
         var preferences = new Preferences();
         var params = req.body;
 
@@ -15,7 +15,7 @@ var controller = {
         preferences.nameBankAccount = params.nameBankAccount;
         preferences.CBUBank = preferences.CBUBank;
         preferences.boxTypeBank = params.boxTypeBank;
-        
+
         preferences.terminosYCondiciones = params.terminosYCondiciones;
 
         preferences.phoneContact = params.phoneContact;
@@ -28,59 +28,57 @@ var controller = {
         preferences.firstColor = params.firstColor;
         preferences.secondColor = params.secondColor;
         preferences.imageBanner = params.imageBanner;
-        
 
-        preferences.save((err, preferencesStored) =>{
-            if(err) return res.status(500).send({message: 'Error al guardar las preferencias.'})
-            if(!preferencesStored) return res.status(404).send({message: 'No se han podido guardar las preferencias'})
-            return res.status(200).send({preferences: preferencesStored});
+
+        preferences.save((err, preferencesStored) => {
+            if (err) return res.status(500).send({ message: 'Error al guardar las preferencias.' })
+            if (!preferencesStored) return res.status(404).send({ message: 'No se han podido guardar las preferencias' })
+            return res.status(200).send({ preferences: preferencesStored });
         });
     },
 
-    getPreferences: function(req, res){
+    getPreferences: function (req, res) {
         var preferencesId = req.params.id;
-        if(preferencesId === null) return res.status(404).send({message: 'La preferencias no existe.'});
+        if (preferencesId === null) return res.status(404).send({ message: 'La preferencias no existe.' });
 
-        Preferences.findById(preferencesId, (err, preferences) =>{
-            if(err) return res.status(500).send({message: 'Error al devolver los datos.'});
+        Preferences.findById(preferencesId, (err, preferences) => {
+            if (err) return res.status(500).send({ message: 'Error al devolver los datos.' });
 
-            if(!preferences) return res.status(404).send({message: 'Las preferencias no existe'});
+            if (!preferences) return res.status(404).send({ message: 'Las preferencias no existe' });
 
-            return res.status(200).send({preferences});
+            return res.status(200).send({ preferences });
         })
     },
 
-    getPreferenceses: function(req, res){
-        Preferences.find({}).sort('-name').exec((err, preferenceses) =>{
-            if(err) return res.status(500).send({message: 'Error al devolver los datos'});
-            
-            if(!preferenceses) return res.status(404).send({message: 'No hay categorias para mostrar'});
-            
-            return res.status(200).send({preferenceses});
+    getPreferenceses: function (req, res) {
+        Preferences.find({}).sort('-preference').exec((err, preferenceses) => {
+            if (err) return res.status(500).send({ message: 'Error al devolver los datos' }); console.log('1')
+            if (!preferenceses) return res.status(404).send({ message: 'No hay categorias para mostrar' });
+            return res.status(200).send({ preferenceses });
         })
     },
-    
-    updatePreferences: function(req, res){
+
+    updatePreferences: function (req, res) {
         var preferencesId = req.params.id;
         var update = req.body;
 
-        Preferences.findByIdAndUpdate(preferencesId, update, {new:true}, (err, preferencesUpdated) =>{
-            if(err) return res.status(500).send({message: 'Error al actualizar.'});
-            if(!preferencesUpdated) return res.status(404).send({message: 'No existen las preferencias ha actualizar'});
+        Preferences.findByIdAndUpdate(preferencesId, update, { new: true }, (err, preferencesUpdated) => {
+            if (err) return res.status(500).send({ message: 'Error al actualizar.' });
+            if (!preferencesUpdated) return res.status(404).send({ message: 'No existen las preferencias ha actualizar' });
             return res.status(200).send({
                 preferences: preferencesUpdated
             })
         });
     },
 
-    
-    deletepreferences: function(req, res){
+
+    deletepreferences: function (req, res) {
         var preferencesId = req.params.id;
-        
-        Preferences.findByIdAndRemove(preferencesId, (err, preferencesRemoved)=>{
-            if(err) return res.status(500).send({message: 'No se han podido eliminar las preferencias'});
-            
-            if(!preferencesRemoved) return res.status(404).send({message: 'No se puede eliminar esa preferencias'});
+
+        Preferences.findByIdAndRemove(preferencesId, (err, preferencesRemoved) => {
+            if (err) return res.status(500).send({ message: 'No se han podido eliminar las preferencias' });
+
+            if (!preferencesRemoved) return res.status(404).send({ message: 'No se puede eliminar esa preferencias' });
 
             return res.status(200).send({
                 preferences: preferencesRemoved
--- routes/preferences.js
+++ routes/preferences.js
@@ -6,7 +6,7 @@ var routerPreferences = express.Router();
 
 routerPreferences.post('/save-preferences', PreferencesController.savePreferences);
 routerPreferences.get('/preferences/:id', PreferencesController.getPreferences);
-routerPreferences.get('preferenceses', PreferencesController.getPreferenceses);
+routerPreferences.get('/preferenceses', PreferencesController.getPreferenceses);
 routerPreferences.put('/preferences/:id', PreferencesController.updatePreferences);
 
 module.exports = routerPreferences;
